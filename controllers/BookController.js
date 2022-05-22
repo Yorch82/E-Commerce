d53e872ad3439832e3a3576fbab863aced60cb9a
Yorch82 <jcampo1982@outlook.com>
2022-05-22 11:21:46 +0200

    Endpoints getAll, delete y getById de Books

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -6,6 +6,38 @@ const BookController = {
             .then(book => res.status(201).send({ message: 'Libro añadido exitosamente.', book }))
             .catch(console.error)
     },
+    getAll(req, res) {
+        Book.findAll({
+          include: [],
+        })
+          .then((books) => res.send(books))
+          .catch((err) => {
+            console.log(err);
+            res.status(500).send({
+              message: "Ha habido un problema al cargar los libros",
+            });
+          });
+    },
+    async delete(req, res) {
+        await Book.destroy({
+          where: {
+            id: req.params.id,
+          },
+        });
+        res.send("El libro ha sido eliminada con éxito");
+      },
+    getById(req, res) {
+    Book.findByPk(req.params.id, {
+      include: [],
+    })
+      .then((book) => res.send(book))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send({
+          message: "Ha habido un problema al cargar el libro",
+        });
+      });
+  },
 
 }
 
--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const BookController = require('../controllers/BookController')
 
-router.post('/addBooks', BookController.addBook)
+router.post('/addBooks', BookController.addBook);
+router.get('/getBooks', BookController.getAll);
+router.delete('/deleteBook/id/:id', BookController.delete);
+router.get("/id/:id", BookController.getById);
 
 module.exports = router;
