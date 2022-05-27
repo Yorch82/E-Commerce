0c13457032a43b77874158664d8e1046492112a1
Yorch82 <jcampo1982@outlook.com>
2022-05-27 09:42:22 +0200

    arreglos multer

--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,7 @@ const BookController = require('../controllers/BookController');
 const { authentication, isAdmin, isManager, isUser } = require('../middlewares/authentication');
 const { upload } = require('../middlewares/multer');
 
-router.post('/addBooks',authentication,isManager, upload.single('myFile'), BookController.addBook);
+router.post('/addBooks', authentication, isManager, upload.single('myFile'), BookController.addBook);
 router.get('/getBooks',authentication, isUser, BookController.getAll);
 router.delete('/deleteBook/id/:id',authentication, isManager, BookController.delete);
 router.get("/id/:id",authentication, isUser, BookController.getById);
